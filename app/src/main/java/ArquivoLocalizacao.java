baaa4c9884ba7e3fca015f3f04e6bb8a574e5f44
Bernardo Victor <bernardo6526@gmail.com>
2019-06-01 20:02:08 -0300

    Camera intent

---
+++ app/src/main/java/ArquivoLocalizacao.java
@@ -0,0 +1,28 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class ArquivoLocalizacao {
+    /*
+    void ArquivoLocalizacao() {
+        BufferedWriter writer = new BufferedWriter(
+                new FileWriter("localizacao.txt", true)  //Set true for append mode
+        );
+
+        writer.close();
+    }
+
+    void inserirLoc(String loc){
+
+        BufferedWriter writer = new BufferedWriter(
+                new FileWriter("localizacao.txt", true)  //Set true for append mode
+        );
+
+        writer.write(loc);
+        writer.newLine();
+        writer.close();
+    }
+
+
+    */
+
+}
--- app/src/main/java/com/example/classphotoholder/menu_principal.java
+++ app/src/main/java/com/example/classphotoholder/menu_principal.java
@@ -1,10 +1,12 @@
 package com.example.classphotoholder;
 
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class menu_principal extends AppCompatActivity {
 
@@ -28,5 +30,23 @@ public class menu_principal extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        ImageButton imgBtnCamera = (ImageButton) findViewById(R.id.imgBtnCamera);
+        imgBtnCamera.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    } // fim do onCreate
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
+
 }

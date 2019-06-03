d77eda52e60d0f944c456ba02ea5aec01f9f1dbe
Bernardo Victor <bernardo6526@gmail.com>
2019-06-03 20:34:55 -0300

    Calendar event and Calendar services

--- app/src/main/java/com/example/classphotofolder/horario.java
+++ app/src/main/java/com/example/classphotofolder/horario.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class horario extends AppCompatActivity {
 
+    TextView txtvHorario = (TextView) findViewById(R.id.txtvHorario);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horario);
     }
+
+    CalendarService.readCalendar(class.this);
 }

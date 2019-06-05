c9987ec83017aa602c7ddf2a136ef63aacd8fe35
Pedro Diógenes <pedroyolo@live.com>
2019-06-05 20:21:15 -0300

    merged 2 screens into one, fixed code, fixed layout

--- app/src/main/java/com/example/classphotofolder/horario.java
+++ app/src/main/java/com/example/classphotofolder/horario.java
@@ -1,5 +1,6 @@
 package com.example.classphotofolder;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class horario extends AppCompatActivity  {
 
@@ -18,17 +21,45 @@ public class horario extends AppCompatActivity  {
     int[] idViews;
     Spinner horarios;
     ImageButton imgBtnInserir, imgBtnExcluir;
+    private static final int horaFim = 1;
+    private static final int horaInicio = 2;
+    TimePickerDialog timePickerDialog;
+    EditText txtNomeDisc, txtHoraIni, txtHoraFim, txtDiaSem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horario);
 
+        txtNomeDisc = (EditText) findViewById(R.id.txtNomeDisciplina);
+        txtHoraIni = (EditText) findViewById(R.id.txtHoraInicio);
+        txtHoraFim = (EditText) findViewById(R.id.txtHoraFim);
+        txtDiaSem = (EditText) findViewById(R.id.txtDiaSemana);
+
+        txtNomeDisc.setHint("Digite o nome da disciplina");
+        txtHoraIni.setHint("Insira a hora de inicio");
+        txtHoraFim.setHint("Insira a hora de fim");
+        txtDiaSem.setHint("Digite o dia da semana dessa aula");
+
+        txtHoraIni.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                throwTimeDialog(horaInicio);
+            }
+        });
+
+        txtHoraFim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                throwTimeDialog(horaFim);
+            }
+        });
+
         cursor = horario.preencheSpinner(); 
         nomeCampos = new String[]{DBHelper.COLUNA_NOME_DISCIPLINA, DBHelper.COLUNA_DIA_SEMANA, DBHelper.COLUNA_HORA_INICIO, DBHelper.COLUNA_HORA_FIM};
         idViews = new int[]{R.id.txtNomeDisciplina, R.id.txtDiaSemana, R.id.txtHoraInicio, R.id.txtHoraFim};
         SimpleCursorAdapter adaptador = new SimpleCursorAdapter(getBaseContext(), R.layout.spinner_horarios, cursor, nomeCampos, idViews, 0);
-        produtos.setAdapter(adaptador);
+        horarios.setAdapter(adaptador);
 
 
         horario = new Controller_Horario(getBaseContext());
@@ -45,6 +76,21 @@ public class horario extends AppCompatActivity  {
                 startActivity(intent);
             }
         });
+    }
+
+    void throwTimeDialog(final int i){
+        timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
+                if(i == horaInicio){
+                    txtHoraIni.setText(hourOfDay + ":" + minutes);
+                }
+                else if(i == horaFim){
+                    txtHoraFim.setText(hourOfDay + ":" + minutes);
+                }
+            }
+        }, 0, 0, false);
+        timePickerDialog.show();
     }
 
 }

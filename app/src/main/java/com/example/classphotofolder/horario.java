8bcab3f1e1dcb2d8fb2a0c6183dff7be401c63ba
Bernardo Victor <bernardo6526@gmail.com>
2019-06-05 21:04:12 -0300

    Spinner dia da semana

--- app/src/main/java/com/example/classphotofolder/horario.java
+++ app/src/main/java/com/example/classphotofolder/horario.java
@@ -1,11 +1,13 @@
 package com.example.classphotofolder;
 
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SimpleCursorAdapter;
@@ -13,49 +15,75 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.app.PendingIntent.getActivity;
+
 public class horario extends AppCompatActivity  {
 
     Controller_Horario horario;
     Cursor cursor;
     String[] nomeCampos;
     int[] idViews;
-    Spinner horarios;
+    Spinner horarios,dias;
     ImageButton imgBtnInserir, imgBtnExcluir;
     private static final int horaFim = 1;
     private static final int horaInicio = 2;
     TimePickerDialog timePickerDialog;
-    EditText txtNomeDisc, txtHoraIni, txtHoraFim, txtDiaSem;
+    EditText editNomeDisc, editHoraIni, editHoraFim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_horario);
 
-        txtNomeDisc = (EditText) findViewById(R.id.txtNomeDisciplina);
-        txtHoraIni = (EditText) findViewById(R.id.txtHoraInicio);
-        txtHoraFim = (EditText) findViewById(R.id.txtHoraFim);
-        txtDiaSem = (EditText) findViewById(R.id.txtDiaSemana);
+        editNomeDisc = (EditText) findViewById(R.id.txtNomeDisciplina);
+        editHoraIni = (EditText) findViewById(R.id.txtHoraInicio);
+        editHoraFim = (EditText) findViewById(R.id.txtHoraFim);
+        dias = (Spinner) findViewById(R.id.spnDiaSemana);
 
-        txtNomeDisc.setHint("Digite o nome da disciplina");
-        txtHoraIni.setHint("Insira a hora de inicio");
-        txtHoraFim.setHint("Insira a hora de fim");
-        txtDiaSem.setHint("Digite o dia da semana dessa aula");
+        editNomeDisc.setHint("Digite o nome da disciplina");
+        editHoraIni.setHint("Insira a hora de inicio");
+        editHoraFim.setHint("Insira a hora de fim");
 
-        txtHoraIni.setOnClickListener(new View.OnClickListener() {
+                // (2) create a simple static list of strings
+        List<String> spinnerArray = new ArrayList<>();
+        spinnerArray.add("Segunda");
+        spinnerArray.add("Terça");
+        spinnerArray.add("Quarta");
+        spinnerArray.add("Quinta");
+        spinnerArray.add("Sexta");
+        spinnerArray.add("Sábado");
+        spinnerArray.add("Domingo");
+        Context c = this;
+        // (3) create an adapter from the list
+        ArrayAdapter<Integer> adapter = new ArrayAdapter<Integer>(
+                c ,
+            android.R.layout.simple_spinner_item,
+            spinnerArray
+        );
+
+        //adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // (4) set the adapter on the spinner
+        dias.setAdapter(adapter);
+
+        editHoraIni.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 throwTimeDialog(horaInicio);
             }
         });
 
-        txtHoraFim.setOnClickListener(new View.OnClickListener() {
+        editHoraFim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 throwTimeDialog(horaFim);
             }
         });
 
-        cursor = horario.preencheSpinner(); 
+        cursor = horario.preencheSpinner();
         nomeCampos = new String[]{DBHelper.COLUNA_NOME_DISCIPLINA, DBHelper.COLUNA_DIA_SEMANA, DBHelper.COLUNA_HORA_INICIO, DBHelper.COLUNA_HORA_FIM};
         idViews = new int[]{R.id.txtNomeDisciplina, R.id.txtDiaSemana, R.id.txtHoraInicio, R.id.txtHoraFim};
         SimpleCursorAdapter adaptador = new SimpleCursorAdapter(getBaseContext(), R.layout.spinner_horarios, cursor, nomeCampos, idViews, 0);
@@ -69,11 +97,17 @@ public class horario extends AppCompatActivity  {
         horario.preencheSpinner();
 
 
-        ImageButton imgBtnInserir = (ImageButton) findViewById(R.id.imgBtnInserir);
+        imgBtnInserir = (ImageButton) findViewById(R.id.imgBtnInserir);
         imgBtnInserir.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), novoHorario.class);
-                startActivity(intent);
+                inserirHorario();
+            }
+        });
+
+        imgBtnExcluir = (ImageButton) findViewById(R.id.imgBtnExcluir);
+        imgBtnExcluir.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                excluirHorario();
             }
         });
     }
@@ -83,14 +117,35 @@ public class horario extends AppCompatActivity  {
             @Override
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
                 if(i == horaInicio){
-                    txtHoraIni.setText(hourOfDay + ":" + minutes);
+                    editHoraIni.setText(hourOfDay + ":" + minutes);
                 }
                 else if(i == horaFim){
-                    txtHoraFim.setText(hourOfDay + ":" + minutes);
+                    editHoraFim.setText(hourOfDay + ":" + minutes);
                 }
             }
         }, 0, 0, false);
         timePickerDialog.show();
+    }
+
+    void inserirHorario(Controller_Horario horario){
+        String resultado;
+        if (!txtNomeDisc.getText().toString().isEmpty() && !txtHoraIni.getText().toString().isEmpty()
+            && !txtHoraFim.getText().toString().isEmpty() && !txtDiaSem.getText().toString().isEmpty()) {
+            String nomeDisc = editNomeDisc.getText().toString();
+            String diaSemana = editDiaSem.getText().toString();
+            //TODO horaInicio
+            //TODO horaFim
+            resultado = horario.insereProduto(nomeDisc, diaSemana, horaInicio, horaFim);
+            Toast.makeText(this, nomeDisc + " no dia " + diaSemana + " inserida com sucesso", Toast.LENGTH_SHORT).show();
+            editNomeDisc.setText(""); editDiaSem.setText(""); editHoraIni.setText(""); editHoraFim.setText("");
+            preencheAdaptador();
+        } else Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+    }
+
+    void excluirHorario(Controller_Horario horario , Spinner horarios){
+        cursor = (Cursor) horarios.getSelectedItem();
+        horario.apagaRegistro(cursor.getInt(0));
+        preencheAdaptador();
     }
 
 }
